414d550d3aa7ab7fe50277e6e9448bced23fe58a
kimgaejin <kimgaejin@naver.com>
2020-10-26 17:54:42 +0900

    전투 테스트용 간단한 코드 삽입
    Q > 속도 2배 곱
    W> 속도 1배로

---
+++ Assets/Scripts/DebugTestTool.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTestTool : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) Time.timeScale *= 2.0f;
+            else if (Input.GetKeyDown(KeyCode.W)) Time.timeScale = 1.0f;
+        }
+    }
+}
