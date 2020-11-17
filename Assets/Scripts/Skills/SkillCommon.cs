0f4c914f5cd8d31ba11e151c3bc68ec624a5c912
kimgaejin <kimgaejin@naver.com>
2020-11-18 02:08:32 +0900

    캐릭터 자리이동 이상한 버그 수정
    캐릭터들이 자기 위치에 앞으로 안나가고 location 1번에 가서 공격하던 것 수정

--- Assets/Scripts/Skills/SkillCommon.cs
+++ Assets/Scripts/Skills/SkillCommon.cs
@@ -33,13 +33,14 @@ public class SkillCommon : MonoBehaviour
     {
         foreach (ChampionInfo target in battleManager.championList)
         {
-            if (target.team == start.team && target.location == 1 && target != start)
+            if (target.team == start.team && target != start)
             {
-                int temp = target.location;
-                target.location = start.location;
-                start.location = temp;
+                if (target.location == 0 || target.location == 3)
+                {
+                    int temp = target.location;
+                    target.location = start.location;
+                    start.location = temp;
 
-                yield return StartCoroutine(battleManager.SwapPosition(start, target));
             }
         }
     }
